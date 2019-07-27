7771d8751eb34c37d887f2810a082321fad00ba5
kokilantsh <iknr6@gmail.com>
2019-07-27 16:44:36 +0530

    hey lead added some conditions

--- BASICSJAVA/src/p1/TESTCLASS2.java
+++ BASICSJAVA/src/p1/TESTCLASS2.java
@@ -2,8 +2,9 @@ package p1;
 
 public class TESTCLASS2 { 
 	public static void main(String[] args) { 
-		// step 2 line 5
-		System.out.println(" hello world 2");
+		// step 2 line 5 and 6
+		System.out.println(" hello world 2"); 
+		// 
 	}
 
 }
